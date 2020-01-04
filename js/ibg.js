5dca0a3b8e58a74619b300de5f4b6821ca1c75d1
Dmytro Demjanenko <47750294+LyaShket@users.noreply.github.com>
2020-01-04 15:55:04 +0200

    Add files via upload
    Починил проект, добавил новую страничку и пофиксил баги и стал натуралом

--- js/ibg.js
+++ js/ibg.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict";
 
 $(document).ready(function(){
@@ -12,19 +11,4 @@ $(document).ready(function(){
 		item.css("background-size", "cover");
 		child.remove();
 	}
-=======
-"use strict";
-
-$(document).ready(function(){
-
-	let ibg = document.getElementsByClassName("ibg");
-	for (let i = 0; i < ibg.length; i++) {
-  	let item = $(ibg[i]);
-  	let child = item.children();
-		let link = child.attr("src");
-		item.css("background", `url('${link}') 50% 50% no-repeat`);
-		item.css("background-size", "cover");
-		child.remove();
-	}
->>>>>>> 8e7d4cb1ab4bf326f2c21edd1f81da1b8f11dcc2
 });

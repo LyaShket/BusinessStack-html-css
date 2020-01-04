0ae2885b41fd6ac6163d1ef5548fb223540ca177
Dmytro Demjanenko <47750294+LyaShket@users.noreply.github.com>
2020-01-04 11:19:09 +0200

    Add files via upload
    Доделал основную blog страницу. Немного переделал шапку (добавил лого ссылку) и еще по мелочевке

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 $(document).ready(function(){
 	$('.header__burger').click(function(event) {
 		$('.header__burger,.header__nav').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+=======
+$(document).ready(function(){
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__nav').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+>>>>>>> 8e7d4cb1ab4bf326f2c21edd1f81da1b8f11dcc2
 });
